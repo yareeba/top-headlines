3157cd28953aea6da0681c3cf10a471e0f7d2ea4
Areeba Yusaf <areeba@MacBook-Pro.local>
2024-03-17 12:13:50 +0000

    added function to retrieve headline data

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,20 @@ export interface CountrySelectOption {
   value: CountryCode;
   label: string;
 }
+
+export interface Headline {
+  title: string;
+  author: string;
+  source: {
+    id: string | null;
+    name: string;
+  };
+  publishedAt: string;
+  url: string;
+}
+
+export interface HeadlinesData {
+  status: string;
+  totalResults: number;
+  articles: Headline[];
+}
